2f292f8544a98e12c6ffa6772a20f9ac0bd5b408
Kai Garcia-Lorincz <106930751+KGL8@users.noreply.github.com>
2024-11-15 23:36:27 -0500

    Add files via upload

--- SIMULATION - PRC 35(6) Stephenson et al/imports.py
+++ SIMULATION - PRC 35(6) Stephenson et al/imports.py
@@ -1,12 +1,12 @@
 # imports.py
 import numpy
-from numpy import array, sin, cos, arccos, sqrt, cross, dot, arctan, tan, mod, pi, append, eye
+from numpy import array, float32, uint32, sin, cos, arccos, sqrt, cross, dot, arctan, tan, mod, pi, append, eye, allclose
 from scipy.integrate import quad
 from numpy.linalg import norm, inv
 from random import random as rnd, uniform as uni
 from dataclasses import dataclass, field
 from params import *
-from helpers import sample_from_pdf
+from helpers import ray_through_aabb, sample_from_pdf, sph_car, rotate_point_to_vector
 from functools import lru_cache
 
 globals().update({name: globals()[name] for name in dir() if not name.startswith('__')})
--- SIMULATION - PRC 35(6) Stephenson et al/params.py
+++ SIMULATION - PRC 35(6) Stephenson et al/params.py
@@ -8,8 +8,24 @@ mn = 939.57     # mass of the neutron (MeV/c^2)
 md = 1875.61    # mass of the deuteron (MeV/c^2)
 
 # Computed values
-v = p / (p + md)   # Velocity of the CM frame
+v = p / (p + md)        # velocity of the CM frame
 γ = 1 / sqrt(1 - v**2)  # Lorentz factor
+
+# Misc
+γErange = (2.5,20)  # For simulation: we are only interested in firing off photons with γE in γErange.
+
+# Physical Functions
+def nθCM_PDF(x):
+    return 3/(x**2+1)
+
+def γ_depth_PDF(x):
+    return 1/(2*x+1)
+
+def γ_E_lvl_PDF(x):
+    return 1/(x+0.1)
+
+def γ_E_to_θ(x):
+    return 1/(x+0.1)
 
 # Derived momentum for neutron and proton in CM frame
 EγCM = γ * p * (1 - v)
@@ -27,6 +43,6 @@ D_depth = 10     # detector depth along z
 α1 = pi*1/2
 α2 = pi*3/4
 α3 = pi*31/36
-T_min = array([-T_width / 2, -T_height / 2, O_T_dist])
-T_max = array([T_width / 2, T_height / 2, T_depth + O_T_dist])
-θ_max = arctan(sqrt((T_height / 2)**2 + (T_width / 2)**2) / O_T_dist)
+T_min = array([-T_width / 2, -T_height / 2, O_T_dist])                  # maximum point on target
+T_max = array([T_width / 2, T_height / 2, T_depth + O_T_dist])          # minimum point on target
+θ_max = arctan(sqrt((T_height / 2)**2 + (T_width / 2)**2) / O_T_dist)   # maximum theta value on target
